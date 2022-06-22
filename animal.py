320adb4c68010b6c3547e30d574672df5ba40121
unicorndreamlife <104462366+unicorndreamlife@users.noreply.github.com>
2022-06-22 13:43:22 -0700

    Initial commit

---
+++ animal.py
@@ -0,0 +1,8 @@
+Author = "Danielle Sung"
+GitHub_username = "dwsung"
+Date = 6/20/2022
+# Description: Asks the user for their favorite animal and then
+#               prints out that that is their favorite animal
+
+fave_animal = "Unicorn"
+print("Your favorite animal is the " + fave_animal + ".")
